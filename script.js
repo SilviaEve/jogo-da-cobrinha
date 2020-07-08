773823e1773d5b780ace86d83626bbe6fd5ceca6
Silvia Eveline <evelineborges1992@gmail.com>
2020-07-07 22:42:05 -0300

    Definindo movimentos

--- script.js
+++ script.js
@@ -7,18 +7,40 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = "right";
+
 function criarBG() {
     context.fillStyle = "lightpink";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
-    for(i=0; i <snake.length; i++){
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = "#FF00FF";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
 
-criarBG();
-criarCobrinha();
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop(); //Retira o ultimo elemento da cobrinha(array)
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); //Acrescenta a "Cabeça da Cobrinha"
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+
 
